4f97af5d3ca6fa942b0d2bea8220077adacf2b47
Vatsal <shahvatsal21@gmail.com>
2026-08-13 21:46:16 +0530

    Fix test discovery for CI

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom', //fake browser environment for our tests to run
-    setupFiles: './src/setup.js', //run setup file before testing
+    environment: "jsdom",
+    setupFiles: "./src/setup.js",
+    include: ["src/**/*.{test,spec}.{js,jsx,ts,tsx}"],
   },
-})
+});
